359dd2881bf341fbcb5a64215d09677f2e40d1eb
IgorMiranda <igor.miranda@ufvjm.edu.br>
2024-09-18 23:33:22 -0300

    Adição do JWT Token ao cadastar usuario

--- src/VotaFacil.Apllication/Facade/EleitorFacade.cs
+++ src/VotaFacil.Apllication/Facade/EleitorFacade.cs
@@ -8,15 +8,17 @@ namespace VotaFacil.Apllication.Facade
     public class EleitorFacade
     {
         private readonly IEleitorRepositorio _eleitor;
+        private readonly ILoginRepositorio _loginRepositorio;
         private readonly IMapper _mapper;
 
-        public EleitorFacade(IEleitorRepositorio eleitorRepositorio, IMapper mapper)
+        public EleitorFacade(IEleitorRepositorio eleitorRepositorio, IMapper mapper, ILoginRepositorio loginRepositorio)
         {
             _mapper = mapper;
             _eleitor = eleitorRepositorio;
+            _loginRepositorio = loginRepositorio;
         }
 
-        public async Task CadastarEleitor(EleitorDTO eleitor)
+        public async Task<(bool, string)> CadastarEleitor(EleitorDTO eleitor)
         {
             var eleitorModel = new EleitorModel(
                 eleitor.Nome,
@@ -27,6 +29,7 @@ namespace VotaFacil.Apllication.Facade
                 eleitor.Password
             );
             await _eleitor.AdicionarEleitor(eleitorModel);
+            return await _loginRepositorio.Login(eleitor.Username, eleitor.Password);
         }
 
         public async Task AtualizarEleitor(EleitorDTO eleitor)
